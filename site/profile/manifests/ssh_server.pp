d01573c2e72cfd86f5a0412505c93cbaa82134a6
kalfapopperis <56482307+kalfapopperis@users.noreply.github.com>
2019-10-19 22:38:38 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDzNEB3PO4NjoAI73fWiJqRN/9cM4ZcN3mvauNPfjB7fxfJwOpvKLcEu7ZHk4s8WbnXTkJ3GTf+loJvjBQ+Ach2jTz3rE+zuA38Ht6v0QsyP/GK4Rc77pVKe+yRURI+q7apeDBiW4nvAyHqtV2NM8O2YFFvQOHFerLacVx+8ezBZ2MxAnr8rLTOJA8WVwZtzOfgtTQJvOEX1/86cvHZ5FfEgpC67w8bktssk0V+mAo6jorsIYIB9WJHxW3AhU+ADJhetejJ6Su3DLn1yh7rmZgyhJbpcqbTGyInjlO6sML+Ht59d71stJc4RXsp4Tra8BHAMIeu/G0JLvTRZg6Jpdpr',
   }
 }
